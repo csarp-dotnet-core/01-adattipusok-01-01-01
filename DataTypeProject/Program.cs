11de52309118b1dc603c020077eeb56df7576b2c
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-09-09 14:58:13 +0200

    Karakter típus

--- DataTypeProject/Program.cs
+++ DataTypeProject/Program.cs
@@ -11,3 +11,14 @@ Console.WriteLine($"x={x}");
 //Egyéb egész típusok: byte, sbyte, short, ushort, int, uint, long, ulong
 // Byte típus
 byte bájt = 250; // Előjel nélküli 8 bites egész szám(0..255)
+
+// Karakter típus
+char letter = 'A';             // Egyetlen karakter
+char number = '1';             // Karakterként értelmezett szám
+char symbol = '#';             // Speciális szimbólum
+char unicodeChar = '\u03A9';   // Unicode karakter (Ω)
+
+Console.WriteLine("Betű: " + letter);        // Kimenet: Betű: A
+Console.WriteLine("Szám: " + number);        // Kimenet: Szám: 1
+Console.WriteLine("Szimbólum: " + symbol);   // Kimenet: Szimbólum: #
+Console.WriteLine("Unicode karakter: " + unicodeChar);  // Kimenet: Unicode karakter: Ω
