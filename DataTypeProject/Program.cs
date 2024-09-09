6fa8bd059a1357f09114a98db92797d1f7d202f1
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-09-09 14:55:02 +0200

    Egyéb egész típusok

--- DataTypeProject/Program.cs
+++ DataTypeProject/Program.cs
@@ -7,3 +7,7 @@ int x; // deklaráció
 // hibás: unassigned local variable: Console.WriteLine($"x={x}");
 x = 10; // definíció
 Console.WriteLine($"x={x}");
+
+//Egyéb egész típusok: byte, sbyte, short, ushort, int, uint, long, ulong
+// Byte típus
+byte bájt = 250; // Előjel nélküli 8 bites egész szám(0..255)
