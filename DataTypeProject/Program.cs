dbbeb8400f2ce20295169b973b84bdb492e9a34d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-09-09 14:54:17 +0200

    Egész adattípus, deklaráció, definíció

---
+++ DataTypeProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Egész adattípus
+int a = 1, b = 2;
+int x; // deklaráció
+// hibás: unassigned local variable: Console.WriteLine($"x={x}");
+x = 10; // definíció
+Console.WriteLine($"x={x}");
