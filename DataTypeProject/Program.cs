4f4a4e7d42b830ffb5fc8a49159373d240656d75
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-09-09 15:11:57 +0200

    Műveletek egész és valós számokkal

--- DataTypeProject/Program.cs
+++ DataTypeProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 
 // Egész adattípus
@@ -22,3 +24,45 @@ Console.WriteLine("Betű: " + letter);        // Kimenet: Betű: A
 Console.WriteLine("Szám: " + number);        // Kimenet: Szám: 1
 Console.WriteLine("Szimbólum: " + symbol);   // Kimenet: Szimbólum: #
 Console.WriteLine("Unicode karakter: " + unicodeChar);  // Kimenet: Unicode karakter: Ω
+
+// Valós típusok
+float flóath = 6 / 2.3f; // f jelzi, hogy float típus
+flóath = (float) 5 / 4; // csak 7 számjegyig képes pontosan számolni.
+
+double dábül = 6 / 2.3; 
+dábül = 6 / 2.3D; // 15-17 számjegyig képes pontosan számolni.
+
+decimal decimál = 6 / 2.3M; 
+decimál = Convert.ToDecimal(6 / 2.3); // 25-29 számjegyig képes pontosan számolni. 
+decimal price = 99.99m;  // m jelzi, hogy decimal típus
+
+// Műveletek egész számokkal
+a = 12;
+b = 5;
+
+int sum = a + b;         // Összeadás: 15
+int difference = a - b;  // Kivonás: 5
+int product = a * b;     // Szorzás: 50
+int quotient = a / b;    // Osztás: 2
+double remainder = a % b;
+Console.WriteLine();
+Console.WriteLine("Összeg: " + sum);
+Console.WriteLine("Különbség: " + difference);
+Console.WriteLine("Szorzat: " + product);
+Console.WriteLine("Hányados: " + quotient);
+Console.WriteLine("Maradék: " + remainder);
+
+// Műveletek valós számokkal
+double num1 = 5.5;
+double num2 = 3.2;
+double s = num1 + num2;
+double diff = num1 - num2;
+double prod = num1 * num2;
+double quot = num1 / num2;
+double rem = num1 % num2;
+Console.WriteLine();
+Console.WriteLine("Összeg: " + s);
+Console.WriteLine("Különbség: " + diff);
+Console.WriteLine("Szorzat: " + prod);
+Console.WriteLine("Hányados: " + quot);
+Console.WriteLine("Maradék: " + rem);
